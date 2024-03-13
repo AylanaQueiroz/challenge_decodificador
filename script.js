8b40d5a734f039a9d9ab4771484a72865ff1b18b
AylanaQueiroz <aylanaqueiroz@gmail.com>
2024-03-13 12:30:31 -0300

    botao criptografar pegando valor da mensagem campo texto e mostrando a mensagem no campo mensagem criptografada

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // a ação: imagem ficará com display: none, e o campo texto abaixo da imagem receberá um ''innerHtml'' que mostrará o valor do campo textarea.
 
 
-
+const campoTexto = document.getElementById('campo_texto')
 const botaoDo = document.getElementById('do_button');
 const botaoUndo = document.getElementById('undo_button');
 const imagemLupa = document.getElementById('img_lupa');
@@ -11,16 +11,32 @@ const campoMensagem = document.getElementById('msg_cripto');
 const campoMensagem2 =  document.getElementById('msg_cripto_2');
 
 
-botaoDo.addEventListener('click', showMessage)
+botaoDo.addEventListener('click', criptografarMsg)
 
-function showMessage() {
+function criptografarMsg() {
    
-
+  
     imagemLupa.style.display = 'none';
-    campoMensagem.innerHTML= 'teste';
-    campoMensagem2.innerHTML = 'teste2'
+    campoMensagem.style.display = 'none';
+    campoMensagem2.style.display = 'none';
+
+    const newDiv = document.createElement('div');
+    const newParagraph =  document.createElement('div');
+    newDiv.appendChild(newParagraph)
+    newDiv.innerText = campoTexto.value
+    document.getElementById('texto_criptografado').appendChild(newDiv)
+
     
-
-
 }
+
+// Criar uma função 
+// Ligar a função ao botão correspondente no html
+// criar um novo p 
+// add value campoTexto ao novo p criado 
+// capturar texto escrito no campo de input do html
+//Desenvolver a lógica da criptografia
+//retornar na tela o resultado do texto criptografado
+
+
+
 
