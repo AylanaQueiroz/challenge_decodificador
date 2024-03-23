9f55d25ef24c0d183e067c0d935424c897f7c8ad
AylanaQueiroz <aylanaqueiroz@gmail.com>
2024-03-23 10:15:59 -0300

    lógica de copiar em construção

--- script.js
+++ script.js
@@ -12,43 +12,56 @@ let imagemLupa = document.getElementById('img_lupa');
 let campoMensagem = document.getElementById('msg_cripto');
 let campoMensagem2 = document.getElementById('msg_cripto_2');
 let testeMsg = document.getElementById('div_msg');
+let botaoCopy = document.getElementById('tcpy');
 
 
 botaoDo.addEventListener('click', criptografar)
 
 function criptografar() {
 
-    imagemLupa.style.display = 'none';
-    campoMensagem.style.display = 'none';
-    campoMensagem2.style.display = 'none';
+    imagemLupa.style.visibility = 'hidden';
+    campoMensagem.style.visibility = 'hidden';
+    campoMensagem2.style.visibility = 'hidden';
+
 
     let newDiv = document.createElement('div')
     let newParagraph = document.createElement('p');
     newDiv.appendChild(newParagraph);
     testeMsg.appendChild(newDiv);
     testeMsg.innerText = campoTexto.value;
-  
     let msgmInput = campoTexto.value;
     let msgCriptografada = msgmInput.replace(/\a+/g, 'ai').replace(/\e+/g, 'enter').replace(/\i+/g, 'imes').replace(/\o+/g, 'ober').replace(/\u+/g, 'ufat')
-    testeMsg.innerText = msgCriptografada
+    testeMsg.innerText = msgCriptografada;
+
+
+    botaoCopy.addEventListener('click', copy)
+
+    function copyMsgCriptografada() {
+        var copyText = campoTexto.value;
+        
+    
+        navigator.clipboard.writeText(msgCriptografada)
+    
+        console.log(msgCriptografada)
+    
+    }
+
+
 
 }
 
 function descriptografar() {
-    let text = "";
-
+    imagemLupa.style.visibility = 'hidden';
+    campoMensagem.style.visibility = 'hidden';
+    campoMensagem2.style.visibility = 'hidden';
     console.log(campoTexto.value);
-    console.log(typeof campoTexto.value)
-
-
-    var texto = campoTexto.value;
-    texto1 = texto.toLowerCase()
-    var resultado = texto1.replace(/\a+/g, 'ai').replace(/\e+/g, 'enter').replace(/\i+/g, 'imes').replace(/\o+/g, 'ober').replace(/\u+/g, 'ufat')
-    console.log(resultado)
+    console.log(typeof campoTexto.value);
+    var texto1 = campoTexto.value;
+    //texto1 = texto.toLowerCase()
+    var resultado = texto1.replace(/\ai+/g, 'a').replace(/\enter+/g, 'e').replace(/\imes+/g, 'i').replace(/\ober+/g, 'o').replace(/\ufat+/g, 'u')
+    testeMsg.innerText = resultado
 
 }
-
-
 
 
 
